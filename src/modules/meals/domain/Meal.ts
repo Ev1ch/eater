1fbfe4922988009bfef6b2591ac4a0aab1638332
Vlad Zusko <vladzvu11@gmail.com>
2023-02-01 01:39:48 +0200

    feat(meals): add normalized meal type

--- src/modules/meals/domain/Meal.ts
+++ src/modules/meals/domain/Meal.ts
@@ -1,19 +1,31 @@
 import type { Ingredient, Amount } from '#/ingredients/domain';
-
+import type { Area } from '@/modules/areas/domain';
+import type { Category } from '@/modules/categories/domain';
 import type Tag from './Tag';
 
 export interface MealIngredient {
+  id: string;
   amount: Amount;
   ingredient: Ingredient;
 }
 
-export default interface Meal {
+interface MealBase {
   id: string;
   name: string;
-  category: string;
-  area: string;
   instructions: string;
   image: string;
+}
+
+export interface NormalizedMeal extends MealBase {
+  category: string;
+  area: string;
+  ingredients: { id: string; ingredient: string; amount: Amount }[];
+  tags: string[];
+}
+
+export default interface Meal extends MealBase {
+  category: Category;
+  area: Area;
   ingredients: MealIngredient[];
   tags: Tag[];
 }
--- src/modules/meals/domain/index.ts
+++ src/modules/meals/domain/index.ts
@@ -1,1 +1,2 @@
-export type { default as Meal } from './Meal';
+export type { default as Meal, NormalizedMeal, MealIngredient } from './Meal';
+export type { default as Tag } from './Meal';
