0c59be6a4e2d269d2c24154252f242e19c3224c4
Vlad Zusko <vladzvu11@gmail.com>
2023-02-01 01:36:52 +0200

    fix(ingredients): fix enum export

--- src/modules/ingredients/domain/index.ts
+++ src/modules/ingredients/domain/index.ts
@@ -1,2 +1,2 @@
 export type { default as Ingredient } from './Ingredient';
-export type { default as Amount, AmountType } from './Amount';
+export { type default as Amount, AmountType } from './Amount';
