00271bb8d2922378de11fc52993742cc624bc606
Vlad Zusko <vladzvu11@gmail.com>
2023-01-31 21:20:05 +0200

    feat(meals): update model

--- src/modules/meals/domain/Meal.ts
+++ src/modules/meals/domain/Meal.ts
@@ -1,3 +1,11 @@
+import type Tag from './Tag';
+
 export default interface Meal {
   id: string;
+  name: string;
+  category: string;
+  area: string;
+  instructions: string;
+  image: string;
+  tags: Tag[];
 }
