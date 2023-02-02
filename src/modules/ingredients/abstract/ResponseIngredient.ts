4abb34778b31821fbbcbf8ff9ee367787866abbd
Vlad Zusko <vladzvu11@gmail.com>
2023-02-02 13:30:44 +0200

    feat(ingredients): delete abstracts

--- src/modules/ingredients/abstract/ResponseIngredient.ts
+++
@@ -1,3 +0,0 @@
-export default interface ResponseIngredient {
-  id: string;
-}
--- src/modules/ingredients/abstract/index.ts
+++
@@ -1,6 +0,0 @@
-export type { default as ResponseIngredient } from './ResponseIngredient';
-export type {
-  default as IIngredientsService,
-  IGetAllResponse,
-  IGetByIdResponse,
-} from './IIngredientsService';
