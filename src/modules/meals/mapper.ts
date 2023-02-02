734c5e27a2f2e9f9a68cf1aea1540abb136b11e6
Vlad Zusko <vladzvu11@gmail.com>
2023-02-02 13:27:47 +0200

    feat(meals): delete mapper

--- src/modules/meals/mapper.ts
+++
@@ -1,6 +0,0 @@
-import type { ResponseMeal } from './abstract';
-import type { Meal } from './domain';
-
-export default function mapToMeal(responseMeal: ResponseMeal): Meal {
-  return { id: responseMeal.id };
-}
