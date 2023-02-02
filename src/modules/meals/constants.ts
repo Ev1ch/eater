e851de29f408bcf745f7609c3547953d9cc79aea
Vlad Zusko <vladzvu11@gmail.com>
2023-02-02 13:28:34 +0200

    feat(meals): add pages size constant

--- src/modules/meals/constants.ts
+++ src/modules/meals/constants.ts
@@ -1,8 +1,1 @@
-import { Meal } from './domain';
-
-export const API_ROOT = '/meals';
-
-export const ROUTES = {
-  all: API_ROOT,
-  one: (id: Pick<Meal, 'id'>) => `${API_ROOT}/${id}`,
-};
+export const PAGE_SIZE = 10;
