57c8e662f1aa241b6e0a9c97585b16f25927a47d
Vlad Zusko <vladzvu11@gmail.com>
2023-02-03 23:46:28 +0200

    feat(ingredients): add basic service types

--- src/modules/ingredients/abstracts/index.ts
+++ src/modules/ingredients/abstracts/index.ts
@@ -1,2 +1,3 @@
 export type { default as FirestoreIngredient } from './FirestoreIngredient';
 export type { default as IngredientReference } from './IngredientReference';
+export * from './Service';
