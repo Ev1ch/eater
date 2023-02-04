d2c4c6338183220b9e652ef54deff1df6de19089
Vlad Zusko <vladzvu11@gmail.com>
2023-02-04 21:33:07 +0200

    fix(fridge): fix fridge content name

--- src/modules/fridge/slice/selectors.ts
+++ src/modules/fridge/slice/selectors.ts
@@ -5,12 +5,12 @@ export const selectFridge = (state: State) => state.fridge.entity;
 
 export const selectFridgeName = (state: State) => selectFridge(state)?.name;
 
-export const selectFridgeContent = (state: State) => selectFridge(state)?.content;
+export const selectFridgeIngredients = (state: State) => selectFridge(state)?.ingredients;
 
 export const selectFridgeIngredientById = (state: State, id: string) =>
-  selectFridgeContent(state)?.find(({ id: ingredientId }) => ingredientId === id);
+  selectFridgeIngredients(state)?.find(({ id: ingredientId }) => ingredientId === id);
 
 export const selectFridgeIngredientsByType = (state: State, type: IngredientType) =>
-  selectFridgeContent(state)?.filter(
+  selectFridgeIngredients(state)?.filter(
     ({ ingredient: { type: ingredientType } }) => ingredientType.id === type.id,
   );
