a652abcc0c46f3503b478e42262fde97bbc1b437
Vlad Zusko <vladzvu11@gmail.com>
2023-02-04 15:50:57 +0200

    feat(areas): add basic slice

---
+++ src/modules/areas/slice/selectors.ts
@@ -0,0 +1,7 @@
+import type { State } from '@/store/abstracts';
+
+export const selectAreas = (state: State) => state.areas.entities;
+
+export const selectAreasOptions = (state: State) => state.areas.options;
+
+export const selectAreaById = (state: State, id: string) => selectAreas(state)[id];
