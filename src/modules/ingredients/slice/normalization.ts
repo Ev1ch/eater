1fdf518172616fbef903d1e9a1c550b2fbb9d8e3
Vlad Zusko <vladzvu11@gmail.com>
2023-02-04 21:21:12 +0200

    fix(ingredients): fix normalization entity

--- src/modules/ingredients/slice/normalization.ts
+++ src/modules/ingredients/slice/normalization.ts
@@ -1,7 +1,8 @@
+import { ingredientTypeEntity } from '@/modules/ingredient-types/slice/normalization';
 import { Entity } from '@/modules/normalization/domain';
 import type { Ingredient } from '../domain';
 
-export const ingredientEntity = new Entity('ingredients');
+export const ingredientEntity = new Entity('ingredients', { type: ingredientTypeEntity });
 
 export interface NormalizedIngredients {
   [id: string]: Ingredient;
