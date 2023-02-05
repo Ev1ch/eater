f483164074d9c041a5f36007603dce7ca8928537
Vasylenko Paul <maincrafter1307@gmail.com>
2023-02-05 18:06:27 +0200

    feat(meals): get list of meals that you can cook

--- src/modules/ingredients/domain/Amount.ts
+++ src/modules/ingredients/domain/Amount.ts
@@ -2,9 +2,48 @@ export enum AmountType {
   G = 'G',
   KG = 'KG',
   L = 'L',
+  ML = 'ML',
 }
 
 export default interface Amount {
   type: AmountType;
   value: string;
 }
+
+const convertToG = (val: Amount) => {
+  const num = Number.parseFloat(val.value);
+
+  if (Number.isNaN(num)) throw new Error('Cannot conmpare NaN value');
+
+  switch (val.type) {
+    case (AmountType.KG):
+    case (AmountType.L):
+      return (1000 * num);
+    case (AmountType.G):
+    case (AmountType.ML):
+    default:
+      return num;
+  }
+};
+
+type CompareOperator = '>' | '>=' | '<' | '<=' | '==';
+
+export const compareAmounts = (a: Amount, b: Amount, operator: CompareOperator = '>=') => {
+  const v1 = convertToG(a);
+  const v2 = convertToG(b);
+
+  switch (operator) {
+    case ('>'):
+      return v1 > v2;
+    case ('>='):
+      return v1 >= v2;
+    case ('<'):
+      return v1 < v2;
+    case ('<='):
+      return v1 <= v2;
+    case ('=='):
+      return v1 === v2;
+    default:
+      throw new Error('Not allowed operator');
+  }
+};
---
+++ src/modules/meals/service/getMealsByIngredients.ts
@@ -0,0 +1,25 @@
+import { getIngredientById } from '#/ingredients/service';
+import type { GetMealsFromIngredients } from '#/meals/abstracts';
+import { compareAmounts } from '@/modules/ingredients/domain/Amount';
+import getMeals from './getMeals';
+
+const getMealsByIngredients: GetMealsFromIngredients = async (ingredients) => {
+  const ingredientsDb = await Promise.all(
+    ingredients.map(async (i) => ({
+      ...i,
+      ingredient: await getIngredientById(i.ingredient),
+    })),
+  );
+  const ingredientsMap: Record<string, typeof ingredientsDb[number]> =
+    ingredientsDb.reduce((acc, curr) => ({ ...acc, [curr.ingredient.id]: curr }), {});
+  const ingredientIds = Object.keys(ingredientsMap);
+
+  const meals = (await getMeals())
+    .filter((meal) => meal.ingredients
+      .every(({ ingredient }) => ingredientIds.includes(ingredient.id)));
+
+  return meals.filter(({ ingredients: mealIngredients }) => mealIngredients
+    .every((i) => compareAmounts(ingredientsMap[i.ingredient.id].amount, i.amount)));
+};
+
+export default getMealsByIngredients;
