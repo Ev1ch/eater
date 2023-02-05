55798d12368c237d539e80712a8e3fa54feda42f
Vlad Zusko <vladzvu11@gmail.com>
2023-02-05 20:45:39 +0200

    feat(fridge): add delete ingredients funcs

--- src/modules/fridge/components/blocks/fridge/FridgeSection.tsx
+++ src/modules/fridge/components/blocks/fridge/FridgeSection.tsx
@@ -1,4 +1,4 @@
-import { useSelector } from '@/store/hooks';
+import { useDispatch, useSelector } from '@/store/hooks';
 import {
   Accordion,
   AccordionSummary,
@@ -6,10 +6,12 @@ import {
   Typography,
   List,
   ListItem,
+  IconButton,
+  Box,
 } from '@/components/common';
-import { ExpandMore as ExpandMoreIcon } from '@/components/icons';
+import { ExpandMore as ExpandMoreIcon, Delete as DeleteIcon } from '@/components/icons';
 import type { IngredientType } from '@/modules/ingredient-types/domain';
-import { selectFridgeIngredientsByType } from '@/modules/fridge/slice';
+import { deleteFridgeIngredientById, selectFridgeIngredientsByType } from '@/modules/fridge/slice';
 import { AMOUNT_TYPE_TO_NAME } from '@/modules/fridge/constants';
 
 interface FridgeSectionProps {
@@ -17,7 +19,12 @@ interface FridgeSectionProps {
 }
 
 export default function FridgeSection({ type }: FridgeSectionProps) {
+  const dispatch = useDispatch();
   const ingredients = useSelector((state) => selectFridgeIngredientsByType(state, type))!;
+
+  const getDeleteIngredientHandler = (id: string) => async () => {
+    await dispatch(deleteFridgeIngredientById(id)).unwrap();
+  };
 
   return (
     <Accordion>
@@ -29,13 +36,20 @@ export default function FridgeSection({ type }: FridgeSectionProps) {
           {ingredients.map(({ id, ingredient, amount }) => (
             <ListItem
               key={id}
-              sx={{ display: 'flex', justifyContent: 'space-between' }}
+              sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}
               disablePadding
             >
               <Typography>{ingredient.name}</Typography>
-              <Typography>
-                {amount.value} {AMOUNT_TYPE_TO_NAME[amount.type]}
-              </Typography>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography sx={{ mr: 1 }}>
+                  {amount.value} {AMOUNT_TYPE_TO_NAME[amount.type]}
+                </Typography>
+                <Box>
+                  <IconButton onClick={getDeleteIngredientHandler(id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </Box>
+              </Box>
             </ListItem>
           ))}
         </List>
