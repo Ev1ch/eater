9b0bb3b33b00a45cdafb87313be53a5ca0f5e60f
Vlad Zusko <vladzvu11@gmail.com>
2023-02-05 11:36:57 +0200

    feat(store): add selector creator

--- src/store/creators/index.ts
+++ src/store/creators/index.ts
@@ -1,2 +1,3 @@
 export { default as createAsyncThunk } from './createAsyncThunk';
 export { default as createAction } from './createAction';
+export { default as createSelector } from './createSelector';
