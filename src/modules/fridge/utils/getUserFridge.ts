a5ca181d8bb8baa632c67ec6932be1913d63fa67
Vasylenko Paul <maincrafter1307@gmail.com>
2023-02-05 13:43:47 +0200

    fix: retur nto currect user

--- src/modules/fridge/utils/getUserFridge.ts
+++ src/modules/fridge/utils/getUserFridge.ts
@@ -2,13 +2,13 @@ import { fridgesCollection, getDocs, query, where } from '@/modules/firebase/fir
 import { getCurrentUser } from '@/modules/user/service';
 
 const getUserFridgeSnap = async () => {
-  // const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-  // if (!currentUser) {
-  //   throw new Error('User is not logged in');
-  // }
+  if (!currentUser) {
+    throw new Error('User is not logged in');
+  }
 
-  const fridgeQuery = query(fridgesCollection, where('userId', '==', '1'));
+  const fridgeQuery = query(fridgesCollection, where('userId', '==', currentUser.id));
   const snapshot = await getDocs(fridgeQuery);
   if (!snapshot.size) {
     throw new Error('User fridge is not created');
