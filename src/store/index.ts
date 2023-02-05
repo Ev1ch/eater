7f879e906d420be39a7d0fd7cdfbcdde3565fe58
Vlad Zusko <vladzvu11@gmail.com>
2023-02-05 13:02:10 +0200

    feat(store): connect basic reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,14 +4,24 @@ import { createWrapper } from 'next-redux-wrapper';
 import environment from '@/environment/client';
 import fridgeReducer from '#/fridge/slice';
 import uiReducer from '#/ui/slice';
-// import mealsReducer from '#/meals/slice';
+import areasReducer from '#/areas/slice';
+import categoriesReducer from '#/categories/slice';
+import ingredientTypesReducer from '#/ingredient-types/slice';
+import ingredientsReducer from '#/ingredients/slice';
+import tagsReducer from '#/tags/slice';
+import mealsReducer from '#/meals/slice';
 
 export const createStore = () =>
   configureStore({
     reducer: {
       ui: uiReducer,
-      // meals: mealsReducer,
       fridge: fridgeReducer,
+      areas: areasReducer,
+      categories: categoriesReducer,
+      ingredientTypes: ingredientTypesReducer,
+      ingredients: ingredientsReducer,
+      tags: tagsReducer,
+      meals: mealsReducer,
     },
     devTools: environment.isDevelopment,
   });
