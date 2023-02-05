ddaf28f733244c54ac38cd30701f4a212947dc0c
Vasylenko Paul <maincrafter1307@gmail.com>
2023-02-05 21:21:12 +0200

    feat(meal): add meal service

--- src/modules/meals/service/index.ts
+++ src/modules/meals/service/index.ts
@@ -1,3 +1,5 @@
 export { default as getMeals } from './getMeals';
 export { default as getMealsByIngredients } from './getMealsByIngredients';
 export { default as getMissingIngredients } from './getMissingIngredients';
+export { default as getMealByRef } from './getByRef';
+export { default as addMeal } from './addMeal';
