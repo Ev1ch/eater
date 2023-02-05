18846db4097fe305767b950a84ba9f7e266a1b1b
Vlad Zusko <vladzvu11@gmail.com>
2023-02-05 12:53:34 +0200

    feat(ingredients): add normalized ingredient type

--- src/modules/ingredients/domain/Ingredient.ts
+++ src/modules/ingredients/domain/Ingredient.ts
@@ -1,8 +1,15 @@
 import type { IngredientType } from '#/ingredient-types/domain';
 
-export default interface Ingredient {
+export interface IngredientBase {
   id: string;
   name: string;
   description: string;
+}
+
+export default interface Ingredient extends IngredientBase {
   type: IngredientType;
+}
+
+export interface NormalizedIngredient extends IngredientBase {
+  type: string;
 }
