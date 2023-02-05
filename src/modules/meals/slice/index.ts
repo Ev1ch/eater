a5074080b5ef6f62483ade83ed021fe898498b48
Vlad Zusko <vladzvu11@gmail.com>
2023-02-06 00:42:00 +0200

    feat(meals): add meals pagination

--- src/modules/meals/slice/index.ts
+++ src/modules/meals/slice/index.ts
@@ -1,37 +1,104 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isFulfilled } from '@reduxjs/toolkit';
 
 import { hydrate } from '@/store/actions';
 import { createAsyncThunk } from '@/store/creators';
 import { DEFAULT_PAGE_SIZE } from '../constants';
-import { NormalizedMeal } from '../domain';
+import { NormalizedMeal, NormalizedMealIngredient } from '../domain';
 import * as service from '../service';
 import { NormalizedMeals, normalizeMeals } from './normalization';
+import {
+  selectCanBeCookedNormalizedPages,
+  selectCanBeCookedPageSize,
+  selectLatestNormalizedPages,
+  selectLatestPageSize,
+} from './selectors';
+
+interface MealsPage {
+  meals: string[];
+}
 
 interface MealsSlice {
   entities: NormalizedMeals;
-  options: {
-    page: {
-      size: number;
+  orders: {
+    canBeCooked: {
+      options: {
+        page: {
+          size: number;
+          index: number;
+        };
+      };
+      pages: MealsPage[];
+    };
+    latest: {
+      options: {
+        page: {
+          size: number;
+          index: number;
+        };
+      };
+      pages: MealsPage[];
     };
   };
 }
 
 const initialState: MealsSlice = {
   entities: {},
-  options: {
-    page: {
-      size: DEFAULT_PAGE_SIZE,
+  orders: {
+    canBeCooked: {
+      options: {
+        page: {
+          size: DEFAULT_PAGE_SIZE,
+          index: 1,
+        },
+      },
+      pages: [],
+    },
+    latest: {
+      options: {
+        page: {
+          size: DEFAULT_PAGE_SIZE,
+          index: 1,
+        },
+      },
+      pages: [],
     },
   },
 };
 
 const name = 'meals';
 
-export const getMeals = createAsyncThunk<void, NormalizedMeal[]>(
-  `${name}/getMeals`,
-  async (_, { dispatch }) => {
-    const meals = await service.getMeals();
+export const getLatestMeals = createAsyncThunk<void, NormalizedMeal[]>(
+  `${name}/getLatestMeals`,
+  async (_, { dispatch, getState }) => {
+    const size = selectLatestPageSize(getState());
+    const pages = selectLatestNormalizedPages(getState());
+    const lastId = pages.at(-1)?.meals.at(-1);
+    const meals = await service.getMeals({
+      page: {
+        lastId,
+        size,
+      },
+    });
+    const normalizedMeals = normalizeMeals(dispatch, meals);
+
+    return normalizedMeals;
+  },
+);
+
+export const getMealsByIngredients = createAsyncThunk<NormalizedMealIngredient[], NormalizedMeal[]>(
+  `${name}/getMealsByIngredients`,
+  async (ingredients, { dispatch, getState }) => {
+    const size = selectCanBeCookedPageSize(getState());
+    const pages = selectCanBeCookedNormalizedPages(getState());
+    const lastId = pages.at(-1)?.meals.at(-1);
+    // @ts-expect-error
+    const meals = await service.getMealsByIngredients(ingredients, {
+      page: {
+        lastId,
+        size,
+      },
+    });
     const normalizedMeals = normalizeMeals(dispatch, meals);
 
     return normalizedMeals;
@@ -41,13 +108,43 @@ export const getMeals = createAsyncThunk<void, NormalizedMeal[]>(
 const slice = createSlice({
   name,
   initialState,
-  reducers: {},
+  reducers: {
+    resetCanBeCookedPageIndex(state) {
+      state.orders.canBeCooked.options.page.index = 1;
+    },
+    resetLatestPageIndex(state) {
+      state.orders.canBeCooked.options.page.index = 1;
+    },
+    setCanBeCookedNextPageIndex(state) {
+      state.orders.canBeCooked.options.page.index += 1;
+    },
+    setLatestNextPageIndex(state) {
+      state.orders.canBeCooked.options.page.index += 1;
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(hydrate, (state, { payload }) => {
         state.entities = payload.meals.entities;
+        state.orders = payload.meals.orders;
       })
-      .addCase(getMeals.fulfilled, (state, { payload }) => {
+      .addCase(getLatestMeals.fulfilled, (state, { payload }) => {
+        const { options } = state.orders.latest;
+        const currentIndex = options.page.index - 1;
+
+        state.orders.latest.pages[currentIndex] = {
+          meals: payload.map(({ id }) => id),
+        };
+      })
+      .addCase(getMealsByIngredients.fulfilled, (state, { payload }) => {
+        const { options } = state.orders.canBeCooked;
+        const currentIndex = options.page.index - 1;
+
+        state.orders.canBeCooked.pages[currentIndex] = {
+          meals: payload.map(({ id }) => id),
+        };
+      })
+      .addMatcher(isFulfilled(getLatestMeals, getMealsByIngredients), (state, { payload }) => {
         payload.forEach((meal) => {
           state.entities[meal.id] = meal;
         });
@@ -55,7 +152,13 @@ const slice = createSlice({
   },
 });
 
-const { reducer } = slice;
+const { reducer, actions } = slice;
 
+export const {
+  resetLatestPageIndex,
+  resetCanBeCookedPageIndex,
+  setLatestNextPageIndex,
+  setCanBeCookedNextPageIndex,
+} = actions;
 export * from './selectors';
 export default reducer;
--- src/modules/meals/slice/selectors.ts
+++ src/modules/meals/slice/selectors.ts
@@ -45,3 +45,85 @@ export const selectMealById = createSelector(
   [selectMeals, (state, id: string) => id],
   (meals, id) => meals[id],
 );
+
+export const selectMealsOrders = (state: State) => state.meals.orders;
+
+export const selectCanBeCookedOrder = createSelector(
+  [selectMealsOrders],
+  (orders) => orders.canBeCooked,
+);
+
+export const selectLatestOrder = createSelector([selectMealsOrders], (orders) => orders.latest);
+
+export const selectCanBeCookedNormalizedPages = createSelector(
+  [selectCanBeCookedOrder],
+  (order) => order.pages,
+);
+
+export const selectLatestNormalizedPages = createSelector(
+  [selectLatestOrder],
+  (order) => order.pages,
+);
+
+export const selectCanBeCookedPages = createSelector(
+  [selectMeals, selectCanBeCookedNormalizedPages],
+  (meals, pages) =>
+    pages.map(({ meals: currentMeals }) => ({
+      meals: currentMeals.map((meal) => meals[meal]),
+    })),
+);
+
+export const selectLatestPages = createSelector(
+  [selectMeals, selectLatestNormalizedPages],
+  (meals, pages) =>
+    pages.map(({ meals: currentMeals }) => ({
+      meals: currentMeals.map((meal) => meals[meal]),
+    })),
+);
+
+export const selectCanBeCookedOptions = createSelector(
+  [selectCanBeCookedOrder],
+  (order) => order.options,
+);
+
+export const selectLatestOptions = createSelector([selectLatestOrder], (order) => order.options);
+
+export const selectCanBeCookedPageSize = createSelector(
+  [selectCanBeCookedOptions],
+  (options) => options.page.size,
+);
+
+export const selectLatestPageSize = createSelector(
+  [selectLatestOptions],
+  (options) => options.page.size,
+);
+
+export const selectCanBeCookedCurrentPageIndex = createSelector(
+  [selectCanBeCookedOptions],
+  (options) => options.page.index,
+);
+
+export const selectLatestCurrentPageIndex = createSelector(
+  [selectLatestOptions],
+  (options) => options.page.index,
+);
+
+export const selectCanBeCookedCurrentPage = createSelector(
+  [selectCanBeCookedPages, selectCanBeCookedCurrentPageIndex],
+  (pages, index) => pages[index - 1],
+);
+
+export const selectLatestCurrentPage = createSelector(
+  [selectLatestPages, selectLatestCurrentPageIndex],
+  (pages, index) => pages[index - 1],
+);
+
+export const selectAllCanBeCookedPagesToCurrent = createSelector(
+  [selectCanBeCookedPages, selectCanBeCookedCurrentPageIndex],
+  (pages, index) => pages.slice(0, index),
+);
+
+export const selectAllLatestPagesToCurrent = createSelector(
+  [selectLatestPages, selectLatestCurrentPageIndex],
+  (pages, index) => pages.slice(0, index),
+);
