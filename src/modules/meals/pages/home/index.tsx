a4378c919377056276e8b2d77b35313cc62e2e37
Vlad Zusko <vladzvu11@gmail.com>
2023-02-05 20:27:07 +0200

    feat(meals): add basic home page

---
+++ src/modules/meals/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import { useSelector } from '@/store/hooks';
+import { Grid } from '@/components/common';
+import { Fridge } from '#/fridge/components/blocks';
+import { selectUser } from '#/user/slice';
+import { MealCard } from '../../components/common';
+import { selectMeals } from '../../slice';
+
+export default function Home() {
+  const user = useSelector(selectUser);
+  const meals = useSelector(selectMeals);
+
+  return (
+    <Grid sx={{ flexGrow: 1 }} container>
+      <Grid sm={user ? 7 : 12} item>
+        <Grid container>
+          {Object.values(meals).map((meal) => (
+            <Grid key={meal.id} xs={12} md={5} item>
+              <MealCard meal={meal} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+      <Grid sx={{ flexGrow: 1, overflow: 'hidden' }} sm={user ? 5 : 0} item>
+        <Fridge />
+      </Grid>
+    </Grid>
+  );
+}
