bcb4cee3f2df6838a976cc9c704c584c5e534762
Vlad Zusko <vladzvu11@gmail.com>
2023-02-05 14:05:02 +0200

    feat(user): update mapper types

--- src/modules/user/mapper.ts
+++ src/modules/user/mapper.ts
@@ -1,8 +1,8 @@
-import type { ResponseUser } from './abstracts';
+import type { FirebaseUser } from '#/firebase/domain';
 import type { User } from './domain';
 import { DEFAULT_USER_NAME } from './constants';
 
-export default function mapToUser(responseUser: ResponseUser): User {
+export default function mapToUser(responseUser: FirebaseUser): User {
   return {
     id: responseUser.uid,
     fullName: responseUser.displayName ?? DEFAULT_USER_NAME,
