ba1c3be018fb53afcef0ac69ca85adb41c00b24c
Vlad Zusko <vladzvu11@gmail.com>
2023-02-06 22:49:16 +0200

    feat(meals): update add types

--- src/modules/meals/abstracts/Service.ts
+++ src/modules/meals/abstracts/Service.ts
@@ -1,8 +1,7 @@
-import type { Paginated } from '#/firebase/abstracts';
-import { Amount } from '@/modules/ingredients/domain';
 import { DocumentReference } from 'firebase/firestore';
-import type { Meal } from '../domain';
-import { MealIngredient, NormalizedMeal, NormalizedMealIngredient } from '../domain/Meal';
+
+import type { Paginated } from '#/firebase/abstracts';
+import type { Meal, MealIngredient, NormalizedMeal, NormalizedMealIngredient } from '../domain';
 
 export type GetMeals = (options?: Paginated) => Promise<Meal[]>;
 
@@ -19,9 +18,10 @@ export type GetMissingIngredients = (
 // Function return meals which were created by current user
 export type GetOwnMeals = (options?: Paginated) => Promise<Meal[]>;
 
-type Ingridients = { ingredient: string; amount: Amount }[];
 export type AddMeal = (
-  meal: Omit<NormalizedMeal, 'id' | 'ingredients'> & { ingredients: Ingridients },
+  meal: Omit<NormalizedMeal, 'id' | 'ingredients'> & {
+    ingredients: Omit<NormalizedMealIngredient, 'id'>;
+  },
 ) => Promise<Meal>;
 
 export type GetMealByRef = (ref: DocumentReference) => Promise<Meal>;
