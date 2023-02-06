83c0f41b08eade17c53fbd6f7f82a6951a8e521c
Vlad Zusko <vladzvu11@gmail.com>
2023-02-06 22:44:13 +0200

    feat: add placeholder image

--- src/modules/meals/constants.ts
+++ src/modules/meals/constants.ts
@@ -1,1 +1,5 @@
-export const DEFAULT_PAGE_SIZE = 10;
+import { DEFAULT_PLACEHOLDER_IMAGE } from '@/core/constants';
+
+export const DEFAULT_PAGE_SIZE = 5;
+
+export const DEFAULT_MEAL_IMAGE = DEFAULT_PLACEHOLDER_IMAGE;
