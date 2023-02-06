6e886f21d75b65eb478c3b29ee7c6e2b31dce884
Vlad Zusko <vladzvu11@gmail.com>
2023-02-06 20:53:25 +0200

    feat(areas): delete mapper

--- src/modules/areas/mapper.ts
+++
@@ -1,6 +0,0 @@
-import type { ResponseArea } from './abstract';
-import type { Area } from './domain';
-
-export default function mapToArea(responseArea: ResponseArea): Area {
-  return { id: responseArea.id };
-}
