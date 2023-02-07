afece71e024c3422256351ebc96aadd82b0d6985
Dmytro <demetrgonchar@gmail.com>
2023-02-07 17:02:11 +0200

    feat: add getIngredientsWithSearch

--- src/environment/server.ts
+++ src/environment/server.ts
@@ -1,12 +1,5 @@
 import * as envalid from 'envalid';
 
-const environment = envalid.cleanEnv(process.env, {
-  API_KEY: envalid.str(),
-  AUTH_DOMAIN: envalid.str(),
-  PROJECT_ID: envalid.str(),
-  STORAGE_BUCKET: envalid.str(),
-  MESSAGING_SENDER_ID: envalid.str(),
-  APP_ID: envalid.str(),
-});
+const environment = envalid.cleanEnv(process.env, {});
 
 export default environment;
